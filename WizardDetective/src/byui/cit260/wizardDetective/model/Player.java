134540f7d6dfce509a45d21ae03feefc68afa905
carathomson <carathomson@10.18.148.48>
2015-01-27 10:02:46 -0700

    Team assignment start

---
+++ WizardDetective/src/byui/cit260/wizardDetective/model/Player.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.wizardDetective.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author carathomson
+ */
+public class Player implements Serializable{
+    private String name;
+    private double inventory;
+
+    public Player() {
+    }
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(double inventory) {
+        this.inventory = inventory;
+    }
+
+     @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", inventory=" + inventory + '}';
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.name);
+        hash = 97 * hash + (int) (Double.doubleToLongBits(this.inventory) ^ (Double.doubleToLongBits(this.inventory) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.inventory) != Double.doubleToLongBits(other.inventory)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
---
+++ WizardDetective/src/wizarddetective/WizardDetective.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wizarddetective;
+
+import byui.cit260.wizardDetective.model.Player;
+
+/**
+ *
+ * @author carathomson
+ */
+public class WizardDetective {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Player playerOne = new Player();
+        
+        playerOne.setName("Fred Flinstone");
+        playerOne.setInventory(0.00);
+        
+        String playerInfo = playerOne.toString();
+        System.out.println(playerInfo);
+    }
+    
+}

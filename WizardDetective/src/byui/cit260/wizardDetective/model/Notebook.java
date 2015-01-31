5efb2d8e9de2332d49c448e5952fb4961d1516dc
carathomson <carathomson@10.18.149.9>
2015-01-30 19:12:12 -0700

    Merge origin/master
    Conflicts:
    	WizardDetective/src/wizarddetective/WizardDetective.java

--- WizardDetective/src/byui/cit260/wizardDetective/model/Notebook.java
+++ WizardDetective/src/byui/cit260/wizardDetective/model/Notebook.java
@@ -6,6 +6,7 @@
 package byui.cit260.wizardDetective.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
@@ -14,6 +15,11 @@ import java.io.Serializable;
 public class Notebook implements Serializable{
     private String clue;
     private double requiredAmount;
+
+    public Notebook() {
+    }
+    
+    
 
     public String getClue() {
         return clue;
@@ -29,6 +35,37 @@ public class Notebook implements Serializable{
 
     public void setRequiredAmount(double requiredAmount) {
         this.requiredAmount = requiredAmount;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + Objects.hashCode(this.clue);
+        hash = 83 * hash + (int) (Double.doubleToLongBits(this.requiredAmount) ^ (Double.doubleToLongBits(this.requiredAmount) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Notebook other = (Notebook) obj;
+        if (!Objects.equals(this.clue, other.clue)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.requiredAmount) != Double.doubleToLongBits(other.requiredAmount)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Notebook{" + "clue=" + clue + ", requiredAmount=" + requiredAmount + '}';
     }
     
     

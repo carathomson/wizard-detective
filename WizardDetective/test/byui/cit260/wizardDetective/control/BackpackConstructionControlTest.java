16338466698e36e10941fe73edf7f61525aa5dbd
carathomson <carathomson@10.18.150.51>
2015-02-10 16:49:27 -0700

    Merge origin/master
    Conflicts:
    	WizardDetective/src/wizarddetective/WizardDetective.java

--- WizardDetective/test/byui/cit260/wizardDetective/control/BackpackConstructionControlTest.java
+++ WizardDetective/test/byui/cit260/wizardDetective/control/BackpackConstructionControlTest.java
@@ -21,17 +21,128 @@ public class BackpackConstructionControlTest {
      * Test of calcBackpackSize method, of class BackpackConstructionControl.
      */
     @Test
-    public void testCalcBackpackSize() {
-        System.out.println("calcBackpackSize");
-        double height = 0.0;
-        double width = 0.0;
-        double depth = 0.0;
-        BackpackConstructionControl instance = new BackpackConstructionControl();
-        double expResult = 0.0;
-        double result = instance.calcBackpackSize(height, width, depth);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
     
+        //test 1
+    public void testCalcBackpackSize() {
+        System.out.println("testCaseOne");
+        double height = 24.0;
+        double width = 18.0;
+        double depth = 6.0;
+        BackpackConstructionControl instance = new BackpackConstructionControl();
+        double expResult = 2592.0;
+        
+        double result = instance.calcBackpackSize(height, width, depth);
+        assertEquals(expResult, result, 0.001);
+        
+    
+        //test2
+        System.out.println("testCaseTwo");
+        height = -7.0;
+        width = 18.0;
+        depth = 6.0;
+        expResult = -1.0;
+        
+        result = instance.calcBackpackSize(height, width, depth);
+        assertEquals(expResult, result, 0.001);
+        
+        
+        //test 3
+                System.out.println("testCaseThree");
+        height = 24.0;
+        width = 21.0;
+        depth = 6.0;
+        expResult = -2.0;
+        
+        result = instance.calcBackpackSize(height, width, depth);
+        assertEquals(expResult, result, 0.001);
+                       
+        
+        //test 4
+                System.out.println("testCaseFour");
+        height = 24.0;
+        width = 18.0;
+        depth = 13.0;
+        expResult = -3.0;
+        
+        result = instance.calcBackpackSize(height, width, depth);
+        assertEquals(expResult, result, 0.001);
+        
+        
+        //test 5
+                System.out.println("testCaseFive");
+        height = 25.0;
+        width = 18.0;
+        depth = 6.0;
+        expResult = 2700.0;
+        
+        result = instance.calcBackpackSize(height, width, depth);
+        assertEquals(expResult, result, 0.001);
+                
+        
+        //test 6
+                System.out.println("testCaseSix");
+        height = 1.0;
+        width = 18.0;
+        depth = 6.0;
+        expResult = 108.0;
+        
+        result = instance.calcBackpackSize(height, width, depth);
+        assertEquals(expResult, result, 0.001);
+                
+        
+        //test 7
+                System.out.println("testCaseSeven");
+        height = 24.0;
+        width = 1.0;
+        depth = 6.0;
+        expResult = 144.0;
+        
+        result = instance.calcBackpackSize(height, width, depth);
+        assertEquals(expResult, result, 0.001);
+                
+        
+        //test 8
+                System.out.println("testCaseEight");
+        height = 24.0;
+        width = 18.0;
+        depth = 1.0;
+        expResult = 432.0;
+        
+        result = instance.calcBackpackSize(height, width, depth);
+        assertEquals(expResult, result, 0.001);
+                
+        
+        //test 9
+                System.out.println("testCaseNine");
+        height = 20.0;
+        width = 20.0;
+        depth = 8.0;
+        expResult = 3200.0;
+        
+        result = instance.calcBackpackSize(height, width, depth);
+        assertEquals(expResult, result, 0.001);
+                
+        
+        //test 10
+                System.out.println("testCaseTen");
+        height = 22.0;
+        width = 16.0;
+        depth = 12.0;
+        expResult = 4224.0;
+        
+        result = instance.calcBackpackSize(height, width, depth);
+        assertEquals(expResult, result, 0.001);
+                
+        
+        //test 11
+                System.out.println("testCaseEleven");
+        height = 25.0;
+        width = 20.0;
+        depth = 12.0;
+        expResult = 6000.0;
+        
+        result = instance.calcBackpackSize(height, width, depth);
+        assertEquals(expResult, result, 0.001);
+                
+    }
 }

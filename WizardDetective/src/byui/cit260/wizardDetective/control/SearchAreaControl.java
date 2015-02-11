3c392caee42bf82aa2db3d9aa0b27665cf2f36c8
carathomson <carathomson@10.18.150.51>
2015-02-11 09:39:13 -0700

    Merge origin/master
    Conflicts:
    	WizardDetective/src/wizarddetective/WizardDetective.java

--- WizardDetective/src/byui/cit260/wizardDetective/control/SearchAreaControl.java
+++ WizardDetective/src/byui/cit260/wizardDetective/control/SearchAreaControl.java
@@ -11,4 +11,20 @@ package byui.cit260.wizardDetective.control;
  */
 public class SearchAreaControl {
     
+    public double calcSearchArea(double time, double speed){
+        
+        if (time < 3 || time > 10) {
+            return -1;
+        }
+        
+        if (speed < 25 || speed > 100) {
+            return -1;
+        }
+            
+        double distance = (speed * time / 60);
+        
+        return distance;
+ 
+    }
+    
 }

5e10e399ca8bc4fe2fe0c95fa9713440ce89baa2
carathomson <carathomson@10.18.149.82>
2015-02-23 09:54:28 -0700

    Merge origin/master
    Conflicts:
    	WizardDetective/src/wizarddetective/WizardDetective.java

--- WizardDetective/src/byui/cit260/wizardDetective/view/GameMenuView.java
+++ WizardDetective/src/byui/cit260/wizardDetective/view/GameMenuView.java
@@ -5,14 +5,94 @@
  */
 package byui.cit260.wizardDetective.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Lynn
  */
 public class GameMenuView {
+    
+        private final String MENU = "\n"
+                +"\n-----------------------------------------------------------"
+                +"\n| Game Menu                                               |"
+                +"\n-----------------------------------------------------------"
+                +"\nI - Inventory"
+                +"\nN - Navigate"
+                +"\nH - Help"
+                +"\nE - Exit"
+                +"\n-----------------------------------------------------------"
+                ;
 
     void displayMenu() {
-       System.out.println("\n*** displayMenu stub function called ***");
+       char selection = ' ';
+        do {
+           System.out.println(MENU); //display the game menu
+           
+           String input = this.getGameMenuInput();//get the user selection
+           selection = input.charAt(0);//get first character of a string
+           
+           this.doAction(selection);//do action based on selection
+           
+        }while (selection != 'E');//a selection is not "Exit"
+        }
+    
+        public String getGameMenuInput() {
+        boolean valid = false; // indicates if the input has been retrieved
+       String getGameMenuInput = null;
+       Scanner keyboard = new Scanner(System.in); // keyboard input stream
+       
+       while(!valid) { //while a valid input has not been retrieved
+           
+           //prompt for menu selection
+           System.out.println("Enter the letter for the Menu Selection");
+           
+           //get the selection from the keyboard and trim off the blanks
+           getGameMenuInput = keyboard.nextLine();
+           getGameMenuInput = getGameMenuInput.trim();
+           
+           // if the selection is invalid
+           if (getGameMenuInput.length() > 1 || getGameMenuInput.length() < 1){
+               System.out.println("Invalid entry must have selection");
+               continue; //repeat again
+               }
+           break; //out of the (exit) the repitition
+       }
+       
+       return getGameMenuInput; // return the input
+    }
+
+         private void doAction(char choice) {
+       
+        switch (choice) {
+            case 'I': // Look at inventory
+                    this.seeInventory();
+                    break;
+            case 'N':// move Locations
+                this.moveLocation();
+                break;
+            case 'H': //pulls up help screen
+                this.displayHelpMenu();
+                break;
+            case 'E': // exit the program
+                return;
+            default:
+                System.out.println("\n*** Invalid Selection *** Try Again");
+                break;
+        }
+    }
+        
+
+    private void seeInventory() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void moveLocation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void displayHelpMenu() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }

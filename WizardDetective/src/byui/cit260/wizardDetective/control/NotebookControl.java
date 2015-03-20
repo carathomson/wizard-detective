b5298eb8e87c6fc46fe515c7a497a9321622ba50
lynnmecham <lynnmecham@192.168.0.11>
2015-03-20 09:44:38 -0600

    Merge origin/master
    Conflicts:
    	WizardDetective/src/byui/cit260/wizardDetective/control/NotebookControl.java

--- WizardDetective/src/byui/cit260/wizardDetective/control/NotebookControl.java
+++ WizardDetective/src/byui/cit260/wizardDetective/control/NotebookControl.java
@@ -33,6 +33,10 @@ public class NotebookControl {
         }
         return array;
     }
+
+    public void display() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
 
    
